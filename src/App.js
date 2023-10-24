c0bb8d582c351f08c6ae95e245dee8d02098c0ff
Sergey87 <serge.techno@gmail.com>
2023-10-24 23:04:00 +0300

    added route in project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
+import Jokes from './pages/Jokes';
+import JokeDetails from './pages/JokeDetails';
+import AddJoke from './pages/AddJoke';
+
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route path='/jokes' exact>
+        <Jokes />
+      </Route>
+      <Route path='/jokes/:jokeId'>
+        <JokeDetails />
+      </Route>
+      <Route path='/add-joke'>
+        <AddJoke />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
